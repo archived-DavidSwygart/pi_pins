3d749047f0b17bc3956be22df4b24c57f0c9d268
davidswygart <davidswygart@gmail.com>
2024-09-26 12:02:59 -0400

    Split arguments to avoid string with spaces

--- setTime.sh
+++ setTime.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+#First positional argument is date (YYYY-MM-DD)
+#Second positional argument is time (HH:MM:SS)
 echo "turning off NTP"
 timedatectl set-ntp no
 sleep .1
-echo "setting time to $1"
-timedatectl set-time "$1"
+echo "setting time to $1 $2"
+timedatectl set-time "$1 $2"
 sleep .1
 echo "turning on NTP"
 timedatectl set-ntp yes
